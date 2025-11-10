7663d5bcc6dc9483edf11711af01fb54c931a863
Omoakinni <ariyotomiwa@gmail.com>
2025-11-10 23:41:30 +0100

    CES 340

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
