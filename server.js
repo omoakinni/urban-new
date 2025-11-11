7897e20282cb9cf9d7ec07fa6a9a00360a23ea4d
Omoakinni <ariyotomiwa@gmail.com>
2025-11-11 22:26:17 +0100

    Assignment 1

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const static = require("./routes/static")
  * View Engine and Templates Routes
  *************************/
 app.set("view engine", "ejs")
-app.use(expressLayouts)
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("layout", "./layouts/layout") // not at views root
 
 // Index route
